7c1f2a6b74a46969ed6b8cb4c7ffb955b809b432
JacobWillis <jacobwillis2012@gmail.com>
2023-04-26 12:39:49 -0400

    'added Models, package.json, and file structure'

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
