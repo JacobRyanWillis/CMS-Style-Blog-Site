18b1ebea225d7808fff3298687c122dd44b00e0f
JacobWillis <jacobwillis2012@gmail.com>
2023-04-27 18:30:32 -0400

    'added routes and updated handlebars'

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+// Get all posts
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll();
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get a single post by id
+router.get('/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Create a new post
+router.post('/', async (req, res) => {
+  try {
+    const postData = await Post.create(req.body);
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update a post by id
+router.put('/:id', async (req, res) => {
+  try {
+    const postData = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!postData[0]) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.status(200).json({ message: 'Post updated successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete a post by id
+router.delete('/:id', async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+    res.status(200).json({ message: 'Post deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,6 +15,45 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Display a single post and its comments
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: Comment, include: [User] }, User],
+    });
+    const post = postData.get({ plain: true });
+    res.render('single-post', { post });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Display the form for creating a new post
+router.get('/new-post', withAuth, (req, res) => {
+  res.render('new-post');
+});
+
+// Display the form for editing an existing post
+router.get('/edit-post/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: User,
+    });
+    const post = postData.get({ plain: true });
+
+    if (post.user_id !== req.session.user_id) {
+      res.status(403).json('You are not authorized to edit this post');
+    } else {
+      res.render('edit-post', { post });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
 router.get("/login", (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
@@ -30,3 +69,5 @@ router.get("/signup", (req, res) => {
 });
 
 module.exports = router;
+
+//  combine signup/login handlebars and edit/new-post handlebars.
