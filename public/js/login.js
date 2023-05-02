144297bd80b16b9a8020350e7a5921800b546d8e
JacobWillis <jacobwillis2012@gmail.com>
2023-05-02 18:50:53 -0400

    'fixed bugs in handlebars'

--- public/js/login.js
+++ public/js/login.js
@@ -26,7 +26,7 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
+    const username = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
@@ -36,9 +36,8 @@ const loginFormHandler = async (event) => {
         body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
       if (response.ok) {
-        document.location.replace('/homepage');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
